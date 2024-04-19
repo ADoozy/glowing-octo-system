b5fa1cf96a4fde82c56e44710c45891c38fcb68b
Dan Vantari Das Manaol <dlmanaolpaterostechnologicalcollege.edu.ph>
2024-04-19 16:59:20 +0800

    added three lines for ptc

--- Prog1.java
+++ Prog1.java
@@ -1,8 +1,6 @@
 public class Prog1 {
 	public static void main(String [] args) {
-		System.out.println("PATEROS");
-		System.out.println("TECHNOLOGICAL");
-		System.out.println("COLLEGE");
+		System.out.println("PATEROS TECHNOLOGICAL COLLEGE");
 
 	}
 }
